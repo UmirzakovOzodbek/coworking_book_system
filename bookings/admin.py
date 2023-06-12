69c493a166e7a2b32ae3479867f882860aec39c4
UmirzakovOzodbek <122611764+umirzakovozodbek@users.noreply.github.com>
2023-06-12 17:01:53 +0500

    Initial commit .gitignore

---
+++ bookings/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+from bookings.models import Book, Resident
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Resident)
+class ResidentAdmin(admin.ModelAdmin):
+    pass
